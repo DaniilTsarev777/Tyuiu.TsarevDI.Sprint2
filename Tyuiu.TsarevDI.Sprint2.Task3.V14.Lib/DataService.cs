1975ddd9c3db7b9eac58f82c55392a707c97d390
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-06 18:28:42 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint2.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Design;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.TsarevDI.Sprint2.Task3.V14.Lib
+{
+    public class DataService : ISprint2Task3V14
+    {
+        public double Calculate(double x)
+
+        {
+            double y;
+            y = 0;
+            if (x > 1)
+                y = Math.Round(x - Math.Pow(((x + 1) / (x - 1)), 3), 3);
+
+            else if (x == 0)
+                y =  Math.Round((3 * x * x - Math.Cos(Math.Pow(x, 3)) + 10)/ (4 * x * x - Math.Sin(Math.Pow(x, 4)) + 12), 3);
+
+            else if ((x > -18) && (x < 2))
+                y =  Math.Round(Math.Pow((1 + (1 / (x * x))), x), 3);
+
+            else if (x < -18)
+                y = Math.Round(x + 10 * x - (1 / x), 3);
+
+            return y;
+
+                
+
+                
+        }   
+            
+    }
+}
---
+++ Tyuiu.TsarevDI.Sprint2.Task3.V14/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.TsarevDI.Sprint2.Task3.V14.Lib;
+namespace Tyuiu.TsarevDI.Sprint2.Task3.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #2                                                             *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                   *");
+            Console.WriteLine("* Задание #3                                                            *");
+            Console.WriteLine("* Варинат #14                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y    *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит  *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение     *");
+            Console.WriteLine("* до трех знаков после запятой;                                         *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            double x;
+            Console.WriteLine("Введите Число: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            
+            Console.WriteLine("Значение функции = "+ds.Calculate(x));
+            Console.ReadKey();
+
+        }
+    }
+}
