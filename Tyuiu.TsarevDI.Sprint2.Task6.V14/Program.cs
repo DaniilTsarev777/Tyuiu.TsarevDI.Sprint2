08977b218c2116a76ed8ca1f19c6a7ddf551f6f6
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-09 19:32:06 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint2.Task6.V14/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.TsarevDI.Sprint2.Task6.V14.Lib;
+namespace Tyuiu.TsarevDI.Sprint2.Task6.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #2                                                             *");
+            Console.WriteLine("* Тема: Получение результата из switch                                  *");
+            Console.WriteLine("* Задание #6                                                            *");
+            Console.WriteLine("* Варинат #14                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+
+            
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+            int k, d;
+            k = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.FindDayName(k, d));
+            Console.ReadKey();
+        }
+    }
+}
