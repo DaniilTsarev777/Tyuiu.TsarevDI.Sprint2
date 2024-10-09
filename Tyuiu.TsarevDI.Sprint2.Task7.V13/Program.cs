555fa81eefbd2478e4c0c1e349eb84dc8f288ee2
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-09 20:10:35 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint2.Task7.V13/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.TsarevDI.Sprint2.Task7.V13.Lib;
+namespace Tyuiu.TsarevDI.Sprint2.Task7.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #2                                                             *");
+            Console.WriteLine("* Тема: обавление к решению итоговых проектов по спринту                *");
+            Console.WriteLine("* Задание #7                                                            *");
+            Console.WriteLine("* Варинат #13                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+            double x, y;
+            Console.WriteLine("Введите значение X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.CheckDotInShadedArea(x, y));
+
+        }
+    }
+}
