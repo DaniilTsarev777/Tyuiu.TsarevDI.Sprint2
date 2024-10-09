21a583eb8ee8226cd07b3acea9b6dd24d15e55c9
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-09 15:15:16 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint2.Task4.V12.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.TsarevDI.Sprint2.Task4.V12.Lib
+{
+    public class DataService : ISprint2Task4V12
+
+    {
+        public double Calculate(double x, double y)
+        
+        {
+            double a, b;
+            
+            a = Math.Pow((7 + (2 / Math.Pow(y, 2))), x);
+            b = ((3 * Math.Pow(x, 2) - Math.Cos(Math.Pow(y, 2))) + 10) / (Math.Pow(y, 2) - Math.Sin(Math.Pow(x, 2)) + 12);
+
+
+            double z = (Math.Sqrt(x) > y * 2) ? a : b;
+
+            return Math.Round(z, 3);
+        }
+    }
+}
---
+++ Tyuiu.TsarevDI.Sprint2.Task4.V12/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.TsarevDI.Sprint2.Task4.V12.Lib;
+namespace Tyuiu.TsarevDI.Sprint2.Task4.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #2                                                             *");
+            Console.WriteLine("* Тема: Тернарный оператор                                              *");
+            Console.WriteLine("* Задание #4                                                            *");
+            Console.WriteLine("* Варинат #12                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значение X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
